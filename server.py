f274fc360e41b903c9eb6fe4e2cc1752b2b52ebd
Dimple-Priya <dimplepriya121@gmail.com>
2023-09-16 08:38:37 +0530

    updated server

--- server.py
+++ server.py
@@ -5,15 +5,21 @@ app = Flask(__name__)
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return 'Hello World'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    n=request.json
+    res=n['first']+n['second']
+
+    return jsonify({'ans':res})
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    n=request.json
+    res=n['first']-n['second']
+
+    return jsonify({'ans':res})
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
